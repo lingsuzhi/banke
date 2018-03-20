b5334ddc65eba1e458f7a52530ac1bcd0b489b44
凌苏志 <554600654@qq.com>
2018-03-20 18:00:34 +0800

    2018年3月20日

---
+++ bankedo/src/main/java/com/lsz/web/IndexController.java
@@ -0,0 +1,21 @@
+package com.lsz.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * Created by ex-lingsuzhi on 2018/3/20.
+ */
+@Controller
+public class IndexController {
+
+
+
+    @RequestMapping("/index.php")
+    public String index(Model model){
+        return "index";
+    }
+}
---
+++ bankedo/src/main/java/com/lsz/web/TestController.java
@@ -0,0 +1,44 @@
+package com.lsz.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by ex-lingsuzhi on 2018/3/20.
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    @RequestMapping("/1.php")
+    @ResponseBody
+    public ResponseEntity customerList(HttpServletRequest request, Model model) {
+        return ResponseEntity.ok("成功123");
+    }
+    @RequestMapping("/freemarker")
+    public String freemarker(Map<String, Object> map){
+        map.put("name", "Joe");
+        map.put("sex", 1);    //sex:性别，1：男；0：女；
+
+        // 模拟数据
+        List<Map<String, Object>> friends = new ArrayList<Map<String, Object>>();
+        Map<String, Object> friend = new HashMap<String, Object>();
+        friend.put("name", "xbq");
+        friend.put("age", 22);
+        friends.add(friend);
+        friend = new HashMap<String, Object>();
+        friend.put("name", "July");
+        friend.put("age", 18);
+        friends.add(friend);
+        map.put("friends", friends);
+        return "freemarker";
+    }
+}
