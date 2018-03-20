8512716e9487767fac5ce6f72a55531c7a50dc91
凌苏志 <554600654@qq.com>
2018-03-20 18:15:23 +0800

    2018年3月20日1

---
+++ bankedo/src/main/java/com/lsz/BankedoApplication.java
@@ -0,0 +1,12 @@
+package com.lsz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankedoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankedoApplication.class, args);
+	}
+}
